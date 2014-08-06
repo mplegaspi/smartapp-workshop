fccce09f786c923303ba6404e0b9cc52f1fa8695
Matt Nohr <matt@smartthings.com>
2014-08-06 11:09:14 -0500

    Add all individual steps

---
+++ steps/05-leave-on-if-still-open.groovy
@@ -0,0 +1,79 @@
+/**
+ *  Turn On Light When Door Opens
+ *
+ *  Copyright 2014 Matt Nohr
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Turn On Light When Door Opens",
+    namespace: "mrnohr",
+    author: "Matt Nohr",
+    description: "Turn on a light when a door opens",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("When the door opens..."){
+        input "contact1", "capability.contactSensor", title: "Where?"
+    }
+    section("Turn on a light..."){
+        input "switches", "capability.switch", multiple: true
+    }
+    section("Turn it off after this many minutes"){
+        input "offMinutes", "number", required: false
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+
+    unsubscribe()
+    unschedule()
+    initialize()
+}
+
+def initialize() {
+    // TODO: subscribe to attributes, devices, locations, etc.
+    subscribe(contact1, "contact", contactHandler)
+}
+
+// TODO: implement event handlers
+def contactHandler(evt) {
+    log.debug "$evt.value: $evt, $settings"
+    state.contactState = evt.value
+
+    if(evt.value == "open") {
+        log.info "Turning on switches: $switches"
+        switches.on()
+
+        if(offMinutes) {
+            runIn(offMinutes * 60, "scheduledTurnOff")
+        }
+    }
+}
+
+def scheduledTurnOff() {
+    if(state.contactState == 'closed') {
+        log.info "Turning off switches after $offMinutes: $switches"
+        switches.off()
+    } else {
+        log.info "Not turning off because the door is still open"
+    }
+}
---
+++ steps/09-pages.groovy
@@ -0,0 +1,105 @@
+/**
+ *  Turn On Light When Door Opens
+ *
+ *  Copyright 2014 Matt Nohr
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Turn On Light When Door Opens",
+    namespace: "mrnohr",
+    author: "Matt Nohr",
+    description: "Turn on a light when a door opens",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    page(name: "mainPage", title: "Turn On Light When It Opens", nextPage: "notificationPage", uninstall:true) {
+        section("When the door opens..."){
+            input "contact1", "capability.contactSensor", title: "Where?"
+        }
+        section("Turn on a light..."){
+            input "switches", "capability.switch", multiple: true
+        }
+        section("Turn it off after this many minutes"){
+            input "offMinutes", "number", required: false
+        }
+    }
+    page(name: "notificationPage", title: "Notifications", install: true) {
+        section("Notification method") {
+            input "pushNotification", "bool", title: "Push notification", required: false
+            input "phone", "phone", title: "Text message at", description: "Tap to enter phone number", required: false
+            input "notificationDelay", "number", title: "Send at most 1 message every X minutes", required: false
+        }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+
+    unsubscribe()
+    unschedule()
+    initialize()
+}
+
+def initialize() {
+    // TODO: subscribe to attributes, devices, locations, etc.
+    subscribe(contact1, "contact.open", contactOpenHandler)
+}
+
+// TODO: implement event handlers
+def contactOpenHandler(evt) {
+    log.debug "$evt.value: $evt, $settings"
+    log.info "Turning on switches: $switches"
+    switches.on()
+
+    if(offMinutes) {
+        runIn(offMinutes * 60, "scheduledTurnOff")
+    }
+
+    //send message if none in past X minutes
+    def recentNotification = false
+    if(notificationDelay) {
+        def timeAgo = new Date(now() - notificationDelay * 60 * 1000)
+        def recentEvents = contact1.eventsSince(timeAgo)
+        recentNotification = recentEvents.count { it.value && it.value == "open" } > 1
+    }
+
+    if((pushNotification || phone) && !recentNotification) {
+        def msg = "You $contact1 is open"
+        log.info "Sending notification $msg"
+        if (pushNotification) {
+            sendPush(msg)
+        }
+        if (phone) {
+            sendSms(phone, msg)
+        }
+    } else {
+        log.info "Not sending notification"
+    }
+}
+
+def scheduledTurnOff() {
+    if(contact1.latestValue("contact") == "closed") {
+        log.info "Turning off switches after $offMinutes: $switches"
+        switches.off()
+    } else {
+        log.info "Not turning off because the door is still open"
+    }
+}
